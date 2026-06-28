2c8cb761909a0d4ebdad09c159868752d80de625
Brendan Hoar <brendan.hoar@gmail.com>
2026-06-28 17:07:51 -0400

    Initialize __init__.py and set very verbose logging

---
+++ __init__.py
@@ -0,0 +1,21 @@
+import gc
+import time
+import ctypes
+import math
+
+import comfy_aimdo.control
+
+#known values of set_log
+#comfy_aimdo.control.set_log_none()
+#comfy_aimdo.control.set_log_critical()
+#comfy_aimdo.control.set_log_error()
+#comfy_aimdo.control.set_log_warning()
+#comfy_aimdo.control.set_log_info()
+#comfy_aimdo.control.set_log_debug()
+#comfy_aimdo.control.set_log_verbose()
+#comfy_aimdo.control.set_log_vverbose()
+
+comfy_aimdo.control.set_log_vverbose()
+
+
+
